fac54599a29e9577740b729ad54b67a108d7c1fe
Jordan Christensen <jchristensen@Jordans-MacBook-Pro.local>
2020-07-17 13:10:09 -0700

    commit

--- js-competencies/arrays-1/arrays.js
+++ js-competencies/arrays-1/arrays.js
@@ -7,6 +7,7 @@
 //"red", "blue", "brown" in the array.
 
 //Code here
+const rubberDucks = ["yellow", "red", "blue", "brown"]
 
 
 //////////////////PROBLEM 2////////////////////
@@ -16,13 +17,13 @@
 //What is the first numbered index of an array?
 
 // var answer = 1
-// var answer = 0
+var answer = 0
 
 //How do you access the length of an array, such as the array
 //you created above called rubberDucks?
 
 // var arrayLength = Array.length(rubberDucks)
-// var arrayLength = rubberDucks.length
+var arrayLength = rubberDucks.length
 // var arrayLength = rubberDucks.len
 // var arrayLength = rubberDucks.length()
 
@@ -35,7 +36,9 @@
 //The passed-in array should NOT be modified.
 
 //Code here
-
+function getLast(arr){
+  return arr[arr.length-1]
+}
 
 //////////////////PROBLEM 4////////////////////
 
@@ -45,7 +48,9 @@
 //The passed-in array should NOT be modified.
 
 //Code here
-
+function getElement(array, index){
+  return array[index];
+}
 
 //////////////////PROBLEM 5////////////////////
 
@@ -55,6 +60,9 @@
 //to the given value. You do not need to return the array.
 
 //Code here
+function changeElement(array, index, value){
+  array[index] = value;
+}
 
 
 //////////////////PROBLEM 6////////////////////
@@ -63,3 +71,7 @@
 //the last element. trim should return the array.
 
 //Code here
+function trim(array){
+  array.pop();
+  return array;
+}
